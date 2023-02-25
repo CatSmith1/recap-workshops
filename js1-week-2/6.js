fd566640015fd91b0fbfd69065176ed0acee1fb5
Artem Filkovskyy <avfilkovsky@gmail.com>
2023-02-25 11:34:45 +0000

    commit

--- js1-week-2/6.js
+++ js1-week-2/6.js
@@ -1,13 +1,8 @@
-
-
-
-
 function printTimesTables(n) {
-    const count = 1;
-    while(count <= 12) {
-        const product = n * i;
-        console.log(`${n} * ${i} = ${product}`);
-        count++;
-    }
+  let count = 1;
+  while (count <= 12) {
+    const product = n * count;
+    console.log(`${n} * ${count} = ${product}`);
+    count++;
+  }
 }
-
